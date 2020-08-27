5d7db5a252d8ed83a59f0c8963207957b83fd8b4
AlexandrB <burdeinick@yandex.ru>
2020-08-27 20:24:15 +0300

    the ferst test ok

--- Application/bot.py
+++ Application/bot.py
@@ -1,10 +1,19 @@
-# import requests
-# import urllib.request
+from flask import Flask, request
 
 
-# url = "https://picsum.photos/1242/2688?grayscale.jpg"
-# img = urllib.request.urlopen(url).read()
+app = Flask(__name__)
 
-# with open("img.jpg", "wb") as inst:
-#     inst.write(img)
+@app.route("/", methods=["GET", "POST"])
+def receive_update():
+    if request.method == "POST":
+        print(request.json)
+    return {"ok": True}
 
+
+
+# curl --location --request POST 'https://api.telegram.org/bot1389628186:AAFwom4Tc69Me6lbm_Iwv3RHIqK3AvzEYGs/setWebhook' \
+# --header 'Content-Type: application/json' \
+# --data-raw '{
+#     "url": "https://501e3667b616.ngrok.io"
+# }'
+# ngrok http 5000
---
+++ main.py
@@ -0,0 +1,19 @@
+import json
+from Application.bot import app
+
+
+def main():
+    app.run(*get_config())
+
+def get_config():
+    with open('Application/config.json') as config:
+        json_str = config.read()
+        json_str = json.loads(json_str)
+
+    host = json_str['server']['host']
+    port = json_str['server']['port']
+    return (host, port)
+
+
+if __name__ == "__main__":
+    main()
