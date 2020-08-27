fd439dc9d0874022672501687a4a848312c93484
Бурдейник Александр Витальевич <burdeinick@yandex.ru>
2020-08-27 16:56:59 +0300

    The beginning of the development hierarchy

---
+++ Application/bot.py
@@ -0,0 +1,10 @@
+# import requests
+# import urllib.request
+
+
+# url = "https://picsum.photos/1242/2688?grayscale.jpg"
+# img = urllib.request.urlopen(url).read()
+
+# with open("img.jpg", "wb") as inst:
+#     inst.write(img)
+
