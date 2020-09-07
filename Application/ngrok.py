51b10e06c1c8e0cb2dcd5ac605635e69e2d44952
Burdeinick <59728916+Burdeinick@users.noreply.github.com>
2020-09-07 21:03:13 +0300

    Delete ngrok.py

--- Application/ngrok.py
+++
@@ -1,8 +0,0 @@
-# it's sure working for webhook set
-curl --location --request POST 'https://api.telegram.org/bot1389628186:AAHS_dnlxLteQI0lOYEc17Ie_Ylhp-9-Ce0/setWebhook' \
---header 'Content-Type: application/json' \
---data-raw '{
-     "url": "https://90714b7d3dc3.ngrok.io"
-}'
-
-ngrok http 5000
